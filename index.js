6c0b5e0afd3645b47db33f7aaedb701347b358a6
Christopher Banta <chris.banta@rocketmail.com>
2023-07-29 11:52:48 -0500

    Added code to generate logo

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const inquirer = require('inquirer');
 const fs = require('fs/promises');
-const shapes = require('./lib/shapes');
+const {Square, Triangle, Circle} = require('./lib/shapes');
 
-
+// w300 h200
+function generateLogo(answers) {
+    return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+    ${answers.shape.render(answers.color)}
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+    </svg>`
+}
 
 function main() {
     inquirer
@@ -28,8 +34,9 @@ function main() {
                 name: 'shape-color',
                 message: 'What color would you like the shape to be?',
             }
-        ]).then(data)
-            fs.writeFile('logo.svg', data, err =>
+        ]).then(answers)
+            let logo = generateLogo(answers)
+            fs.writeFile('logo.svg', logo, err =>
             err ? console.log('Uh-oh, something went wrong.. try again') : console.log('Generated logo.svg'))
     }
 
@@ -39,4 +46,3 @@ function main() {
     // put answers into different functions which return html?
     // put everything together
     // write file
-    
